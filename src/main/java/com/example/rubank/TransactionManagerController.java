f1bc779409bb522c9d35cb26dd72f9a1a99bd58d
Pranay Bhatt <pranay1921@gmail.com>
2023-11-04 22:05:59 -0400

    Wrote close method in controller

--- src/main/java/com/example/rubank/TransactionManagerController.java
+++ src/main/java/com/example/rubank/TransactionManagerController.java
@@ -6,6 +6,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class TransactionManagerController {
+    private static final String CHECKING = "Checking";
+    private static final String COLLEGE_CHECKING = "College Checking";
+    private static final String SAVINGS = "Savings";
+    private static final String MONEY_MARKET = "Money Market";
 
     @FXML
     private TextField fName_open, lName_open, depositAmount_open;
@@ -32,6 +36,11 @@ public class TransactionManagerController {
         lName_open.textProperty().addListener((observable, oldValue, newValue) -> openKeyReleasedProperty());
         dob_open.valueProperty().addListener((observable, oldValue, newValue) -> openKeyReleasedProperty());
         depositAmount_open.textProperty().addListener((observable, oldValue, newValue) -> openKeyReleasedProperty());
+
+        closeButton.setDisable(true);
+        fName_close.textProperty().addListener((observable, oldValue, newValue) -> closeKeyReleasedProperty());
+        lName_close.textProperty().addListener((observable, oldValue, newValue) -> closeKeyReleasedProperty());
+        dob_close.valueProperty().addListener((observable, oldValue, newValue) -> closeKeyReleasedProperty());
     }
 
     @FXML
@@ -39,7 +48,7 @@ public class TransactionManagerController {
         String fName = fName_open.getText();
         String lName = lName_open.getText();
         String dobString = dob_open.getValue().toString();
-        System.out.println(dobString);
+        // System.out.println(dobString);
         Date dob;
         try {
             dob = parseDate(dobString);
@@ -76,8 +85,48 @@ public class TransactionManagerController {
     }
 
     @FXML
+    private Button closeButton;
+    @FXML
+    private TextField fName_close, lName_close;
+    @FXML
+    private RadioButton checking_close, collegeChecking_close, savings_close, moneyMarket_close;
+    @FXML
+    private DatePicker dob_close;
+    @FXML
     private void close() {
+        String fName = fName_close.getText();
+        String lName = lName_close.getText();
+        String dobString = dob_close.getValue().toString();
 
+        Date dob;
+        try {
+            dob = parseDate(dobString);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        Profile profile = new Profile(fName, lName, dob);
+        Account account;
+        String accountTypeString = ((RadioButton) accountType.getSelectedToggle()).getText();
+        switch (accountTypeString) {
+            case CHECKING -> account = new Checking(profile);
+            case COLLEGE_CHECKING -> account = new CollegeChecking(profile);
+            case SAVINGS -> account = new Savings(profile);
+            case MONEY_MARKET -> account = new MoneyMarket(profile);
+            default -> {
+                System.out.println("Invalid account type: " + accountTypeString);
+                return;
+            }
+        }
+        if (database.close(account)) {
+            messageArea.appendText(fName + " " + lName + " " + dobString + "(" + accountTypeString + ") has been closed.\n");
+        }
+        else messageArea.appendText(fName + " " + lName + " " + dobString + "(" + accountType + ") is not in the database.\n");
+    }
+    @FXML
+    private void closeKeyReleasedProperty() {
+        boolean isDisabled = fName_close.getText().isEmpty() || lName_close.getText().isEmpty() || dob_close.getValue() == null;
+        closeButton.setDisable(isDisabled);
     }
 
 }
